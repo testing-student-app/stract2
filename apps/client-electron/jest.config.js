e526528135454d58e5ff987c2fb9c3a1dd44fd53
Reidond <reidond@gmail.com>
2020-09-09 18:27:29 +0300

    initial apps

---
+++ apps/client-electron/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: '.-client-electron',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  coverageDirectory: '../../coverage/apps/client-electron'
+};
---
+++ apps/client/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'client',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/apps/client',
+  snapshotSerializers: ['jest-serializer-vue'],
+  globals: {
+    'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' },
+    'vue-jest': { tsConfig: 'apps/client/tsconfig.spec.json' },
+  },
+};
