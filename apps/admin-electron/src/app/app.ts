6b5eb3c3121470d5f3413cf003ef84abac4e42ba
Reidond <reidond@gmail.com>
2020-09-13 15:53:26 +0300

    feat: add vue-devtools

--- apps/admin-electron/src/app/app.ts
+++ apps/admin-electron/src/app/app.ts
@@ -44,6 +44,9 @@ export default class App {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
+    if (this.isDevelopmentMode()) {
+      BrowserWindow.addDevToolsExtension('node_modules/vue-devtools');
+    }
     App.initMainWindow();
     App.loadMainWindow();
   }
