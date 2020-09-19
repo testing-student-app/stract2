171cb752a0fb96c3c73472f3106829c54608f3a8
Reidond <reidond@gmail.com>
2020-09-19 22:30:11 +0300

    feat: move builders to separate repo

--- libs/electron/README.md
+++
@@ -1,7 +0,0 @@
-# electron
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test electron` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/vue/src/schematics/library/schema.d.ts
+++
@@ -1,9 +0,0 @@
-export interface LibrarySchematicSchema {
-  name: string;
-  directory?: string;
-  skipTsConfig: boolean;
-  skipFormat: boolean;
-  tags?: string;
-  unitTestRunner: 'jest' | 'none';
-  publishable: boolean;
-}
